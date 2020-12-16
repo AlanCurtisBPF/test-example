78a77066a32712cb05fc1418b70c9c94c040d81f
Alan Curtis <alan.curtis@bcfranchise.com>
2020-12-16 13:13:44 -0700

    added counter test multiple actions and changes

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,36 +1,47 @@
-import {useState} from 'react';
+import { useState } from "react";
 import { connect } from "react-redux";
 import {
- increment as incrementAction,
- decrement as decrementAction,
- reset as resetAction,
+  increment as incrementAction,
+  decrement as decrementAction,
+  reset as resetAction,
 } from "../../redux/reducers/counter/counterActions";
 
 const Counter = (props) => {
   const { increment, decrement, reset, count } = props;
-  const [margin , setMargin] = useState(1);
+  const [margin, setMargin] = useState(1);
 
-  const handlePlus = ()=>{
-    increment(margin)
-  }
-  const handleMinus = ()=>{
-    decrement(margin)
-  }
-  const handleReset = ()=>{
-    reset()
-  }
+  const handlePlus = () => {
+    increment(margin);
+  };
+  const handleMinus = () => {
+    decrement(margin);
+  };
+  const handleReset = () => {
+    reset();
+  };
+
+  const handleSetMargin = (e) => {
+    let value = e.target.value;
+    setMargin(value);
+  };
 
   return (
     <div className="Conter">
       Counter Comp
       <div>Count</div>
-      <div data-testid='count' >{count}</div>
+      <div data-testid="count">{count}</div>
       <button onClick={handlePlus}>+</button>
       <button onClick={handleMinus}>-</button>
       <button onClick={handleReset}>reset</button>
       <button onClick={() => console.log(props)}>Log</button>
       <div>margin to increase or decrease by</div>
-      <input type="number" value={margin} onChange={(e)=>setMargin(e.target.value)} />
+      <input
+        type="number"
+        min="1"
+        data-testid="margin"
+        value={margin}
+        onChange={handleSetMargin}
+      />
     </div>
   );
 };
